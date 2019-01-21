029ead7ff61a115a16f5e0db5dd24b99ea70a89a
tanshoudong <2252437684@qq.com>
2019-01-21 23:03:48 +0800

    add

--- Design/Model/ExpandAttackTree.py
+++ Design/Model/ExpandAttackTree.py
@@ -8,6 +8,35 @@ Author: TanShouDong.
 
 """
 
+class TreeNode():
+    """
+    describtion :
+        definition of the node  of the Expand Attack Tree
+    Attribution:
+        mark:
+            Indicates whether the highlight is highlighted
+        fun:
+            Indicates the main function of the function
+        weights:
+            The weight of the node, including leaf nodes and internal nodes
+        stv:
+            Static literature index of nodes
+        leaf:
+            Indicates whether it is a leaf node
+        type:
+            Indicates whether it is a leaf node
+    """
+    def __init__(self):
+        self.mark=None
+        self.fun=""
+        self.weights=0
+        self.stv=0
+        self.leaf=None
+        self.type=""
+
+
+
+
 
 
 
